671dec97e797e36445068f1bca56911c820af14d
low <lowwenjuin27@gmail.com>
2025-04-01 17:06:02 +0800

    refactor: modify the makefile that able to eccept bonus and minilibx

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # MAKEFILE
-this is a Makefile generator that generate a simple makefile for my project.
-(only for executable file)
+this is a Makefile generator that generate a simple makefile for 42 project.
+(not friendly for libft, ft_printf and gnl)
 
 # How to execute
 ```
@@ -8,5 +8,10 @@ step 1. you need to "chmod +x mf_genarator.sh"
 step 2. you're ready to go, just "./mf_generator.sh"
 ```
 
+# ALERT ON LIBFT
+1. libft must combine mandatory and bonus in one makefile (no bonus in makefile)
+2. libft must include ft_printf and gnl
+
 # [Note]
- i had put a explanation/guide for the Makefile in repo, feel free to take a look
+Makefile have two different version that is no bonus and bonus. thier handling is different
+, so i had make some explanation/guide for the Makefile, feel free to take a look
--- mf_genarator.sh
+++ mf_genarator.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILE="./file_standby.txt"
+FILE="Makefile"
 bonus_input=""
 project_input=""
 compiler_input=""
@@ -8,77 +8,205 @@ path_input=""
 
 function ft_input_the_rest()
 {
-	while true; do # Check if the user needs bonus input
-		read -p "Do you need bonus section? (y/n): " bonus_input
-		case $bonus_input in # Check if the user needs bonus input
-			[yY]* ) # If the user needs bonus input
-				echo -en "CFLAGS = -Wall -Wextra -Werror\n\n" >> $FILE
-				echo -en "SRC = \n\n" >> $FILE
-				echo -en "BONUS = \n\n" >> $FILE
-				echo -en "OBJ_FOLDER = object_files\n\n" >> $FILE
-				echo -en "OBJ_SRC = \$(addprefix \$(OBJ_FOLDER)/, \$(SRC:.c=.o))\n\n" >> $FILE
-				echo -en "OBJ_BONUS = \$(addprefix \$(OBJ_FOLDER)/, \$(BONUS:.c=.o))\n\n" >> $FILE
-				echo -en "all : \$(NAME)\n\n" >> $FILE
-				echo -en "\$(NAME) : \$(OBJ_SRC)\n" >> $FILE
-				echo -en "\t\$(CC) \$(CFLAGS) \$(OBJ_SRC) -o \$@\n\n" >> $FILE
-				echo -en "\$(OBJ_FOLDER)/%.o : %.c | \$(OBJ_FOLDER)\n" >> $FILE
-				echo -en "\t\$(CC) \$(CFLAGS) -c $< -o \$@\n\n" >> $FILE
-				echo -en "\$(OBJ_FOLDER):\n" >> $FILE
-				echo -en "\t@mkdir -p \$(OBJ_FOLDER)\n\n" >> $FILE
-				echo -en "clean :\n" >> $FILE
-				echo -en "\trm -rf \$(OBJ_FOLDER)\n\n" >> $FILE
-				echo -en "fclean : clean\n" >> $FILE
-				echo -en "\trm -f \$(NAME)\n\n" >> $FILE
-				echo -en "re : fclean all\n\n" >> $FILE
-				echo -en "bonus : \$(OBJ_BONUS)\n" >> $FILE
-				echo -en "\t\$(CC) \$(CFLAGS) \$(OBJ_BONUS) -o \$(NAME)\n\n" >> $FILE
-				echo -en ".PHONY: all clean fclean re bonus\n" >> $FILE
-				break;;
-			[nN]* ) # If the user does not need bonus input
-				echo -en "CFLAGS = -Wall -Wextra -Werror\n\n" >> $FILE
-				echo -en "SRC = \n\n" >> $FILE
-				echo -en "OBJ_FOLDER = object_files\n\n" >> $FILE
-				echo -en "OBJ_SRC = \$(addprefix \$(OBJ_FOLDER)/, \$(SRC:.c=.o))\n\n" >> $FILE
-				echo -en "all : \$(NAME)\n\n" >> $FILE
-				echo -en "\$(NAME) : \$(OBJ_SRC)\n" >> $FILE
-				echo -en "\t\$(CC) \$(CFLAGS) \$(OBJ_SRC) -o \$@\n\n" >> $FILE
-				echo -en "\$(OBJ_FOLDER)/%.o : %.c | \$(OBJ_FOLDER)\n" >> $FILE
-				echo -en "\t\$(CC) \$(CFLAGS) -c $< -o \$@\n\n" >> $FILE
-				echo -en "\$(OBJ_FOLDER):\n" >> $FILE
-				echo -en "\t@mkdir -p \$(OBJ_FOLDER)\n\n" >> $FILE
-				echo -en "clean :\n" >> $FILE
-				echo -en "\trm -rf \$(OBJ_FOLDER)\n\n" >> $FILE 
-				echo -en "fclean : clean\n" >> $FILE
-				echo -en "\trm -f \$(NAME)\n\n" >> $FILE
-				echo -en "re : fclean all\n\n" >> $FILE
-				echo -en ".PHONY: all clean fclean re\n" >> $FILE
-				break;;
-			* )  # If the user inputs an invalid input
-				echo "Invalid input. Please answer y or n."
-				;;
-		esac
-	done
-}                                 
+	if [[ "$bonus_input" =~ ^[yY]$ ]]; then
+		if [[ "$minilibx_input" =~ ^[yYnN]$ ]]; then # if minilibx is needed
+			echo -en "CFLAGS = -Wall -Wextra -Werror -g\n\n" >> $FILE
+			echo -en "INC = -I include\n\n" >> $FILE
+			echo -en "SRC_DIR = src\n\n" >> $FILE
+			echo -en "BONUS_DIR = bonus\n\n" >> $FILE
+			echo -en "SRC = \$(wildcard \$(SRC_DIR)/*.c)\n\n" >> $FILE
+			echo -en "BONUS = \$(wildcard \$(BONUS_DIR)/*.c)\n\n" >> $FILE
+			echo -en "OBJ_FOLDER = object_files\n\n" >> $FILE
+			echo -en "OBJ_SRC = \$(SRC:%.c=\$(OBJ_FOLDER)/%.o)\n\n" >> $FILE
+			echo -en "OBJ_BONUS = \$(BONUS:%.c=\$(OBJ_FOLDER)/%.o)\n\n" >> $FILE
+			echo -en "LIBFT_DIR = libft/libft.a\n\n" >> $FILE
 
-function ft_check()
-{
-	while true; do
-		if [ -z "$path_input" ]; then
-			path_input="./"  # Default path is the current directory
-		fi
+			echo -en "# check for OS\n" >> $FILE
+			echo -en "ifeq (\$(shell uname), Linux)\n" >> $FILE
+			echo -en "\tMLX_DIR = minilibx-linux\n" >> $FILE
+			echo -en "\tMINILIBX_LIBRARY = -L\$(MINILIBX) -lmlx -L/usr/lib -Imlx_linux -lXext -lX11 -lm -lz\n" >> $FILE
+			echo -en "else\n" >> $FILE
+			echo -en "\tMLX_DIR = minilibx\n" >> $FILE
+			echo -en "\tMINILIBX_LIBRARY = -L\$(MLX_DIR) -lmlx -framework OpenGL -framework AppKit\n" >> $FILE
+			echo -en "endif\n\n" >> $FILE
 
-		if [ -f "$path_input/Makefile" ]; then # Check if Makefile already exists
-			echo "Makefile already exists in $path_input"
-			exit 1
+			printf "GREEN = \\\033[0;32m\n" >> $FILE
+			printf "RESET = \\\033[0m\n\n" >> $FILE
+
+			echo -en "all : \$(NAME)\n\n" >> $FILE
+
+			echo -en "\$(NAME) : \$(OBJ_SRC)\n" >> $FILE
+			echo -en "\tmake -C libft\n" >> $FILE
+			echo -en "\tmake -C \$(MLX_DIR)\n" >> $FILE
+			echo -en "\t\$(CC) \$(CFLAGS) \$(OBJ_SRC) \$(LIBFT_DIR) \$(MLX_DIR) -o \$(NAME)\n\n" >> $FILE
+			echo -en "\t@echo \"\${GREEN}-----COMPILED DONE-----\\\n\${RESET}\"\n\n" >> $FILE
+
+			echo -en "\$(OBJ_FOLDER)/%.o : %.c | \$(OBJ_FOLDER)\n" >> $FILE
+			echo -en "\t\$(CC) \$(CFLAGS) -c \$< -o \$@\n\n" >> $FILE
+
+			echo -en "\$(OBJ_FOLDER):\n" >> $FILE
+			echo -en "\t@mkdir -p \$(OBJ_FOLDER)\n\n" >> $FILE
+
+			echo -en "clean :\n" >> $FILE
+			echo -en "make clean -C libft\n" >> $FILE
+			echo -en "\tmake clean -C \$(MLX_DIR)\n" >> $FILE
+			echo -en "\trm -rf \$(OBJ_FOLDER)\n\n" >> $FILE
+
+			echo -en "fclean : clean\n" >> $FILE
+			echo -en "\tmake fclean -C libft\n" >> $FILE
+			echo -en "\tmake fclean -C \$(MLX_DIR)\n" >> $FILE
+			echo -en "\trm -f \$(NAME)\n\n" >> $FILE
+			echo -en "\t@echo \"\${GREEN}-----REMOVED ALL FILES-----\\\n\${RESET}\"\n\n" >> $FILE
+
+			echo -en "re : fclean all\n\n" >> $FILE
+
+			echo -en "bonus : \$(OBJ_BONUS)\n" >> $FILE
+			echo -en "\t\$(CC) \$(CFLAGS) \$(OBJ_BONUS) \$(LIBFT_DIR) \$(MLX_DIR) -o \$(NAME)\n\n" >> $FILE
+
+			echo -en ".PHONY: all clean fclean re bonus\n" >> $FILE
 		else
-			if mv file_standby.txt "$path_input/Makefile"; then # Check if the path is valid
-				break
-			else
-				echo "Failed to move Makefile to $path_input. Please check the path and try again."
-				read -p "Enter the path again: " path_input
-			fi
+			echo -en "CFLAGS = -Wall -Wextra -Werror\n\n" >> $FILE
+			echo -en "INC = -I include\n\n" >> $FILE
+			echo -en "SRC_DIR = src\n\n" >> $FILE
+			echo -en "BONUS_DIR = bonus\n\n" >> $FILE
+			echo -en "SRC = \$(wildcard \$(SRC_DIR)/*.c)\n\n" >> $FILE
+			echo -en "BONUS = \$(wildcard \$(BONUS_DIR)/*.c)\n\n" >> $FILE
+			echo -en "OBJ_FOLDER = object_files\n\n" >> $FILE
+			echo -en "OBJ_SRC = \$(addprefix \$(OBJ_FOLDER)/, \$(SRC:.c=.o))\n\n" >> $FILE
+			echo -en "OBJ_BONUS = \$(addprefix \$(OBJ_FOLDER)/, \$(BONUS:.c=.o))\n\n" >> $FILE
+			echo -en "LIBFT_DIR = libft/libft.a\n\n" >> $FILE
+
+			printf "GREEN = \\\033[0;32m\n" >> $FILE
+			printf "RESET = \\\033[0m\n\n" >> $FILE
+
+			echo -en "all : \$(NAME)\n\n" >> $FILE
+
+			echo -en "\$(NAME) : \$(OBJ_SRC)\n" >> $FILE
+			echo -en "\tmake -C libft\n" >> $FILE
+			echo -en "\tmake -C \$(MLX_DIR)\n" >> $FILE
+			echo -en "\t\$(CC) \$(CFLAGS) \$(OBJ_SRC) \$(LIBFT_DIR)  -o \$(NAME)\n\n" >> $FILE
+			echo -en "\t@echo \"\${GREEN}-----COMPILED DONE-----\\\n\${RESET}\"\n\n" >> $FILE
+
+			echo -en "\$(OBJ_FOLDER)/%.o : %.c | \$(OBJ_FOLDER)\n" >> $FILE
+			echo -en "\t\$(CC) \$(CFLAGS) -c \$< -o \$@\n\n" >> $FILE
+
+			echo -en "\$(OBJ_FOLDER):\n" >> $FILE
+			echo -en "\t@mkdir -p \$(OBJ_FOLDER)\n\n" >> $FILE
+
+			echo -en "clean :\n" >> $FILE
+			echo -en "make clean -C libft\n" >> $FILE
+			echo -en "\trm -rf \$(OBJ_FOLDER)\n\n" >> $FILE
+
+			echo -en "fclean : clean\n" >> $FILE
+			echo -en "\tmake fclean -C libft\n" >> $FILE
+			echo -en "\trm -f \$(NAME)\n\n" >> $FILE
+			echo -en "\t@echo \"\${GREEN}-----REMOVED ALL FILES-----\\\n\${RESET}\"\n\n" >> $FILE
+
+			echo -en "re : fclean all\n\n" >> $FILE
+
+			echo -en "bonus : \$(OBJ_BONUS)\n" >> $FILE
+			echo -en "\t\$(CC) \$(CFLAGS) \$(OBJ_BONUS) \$(LIBFT_DIR)  -o \$(NAME)\n\n" >> $FILE
+
+			echo -en ".PHONY: all clean fclean re bonus\n" >> $FILE
 		fi
-	done
+	else
+		if [[ "$minilibx_input" =~ ^[yYnN]$ ]]; then # if minilibx is needed
+			echo -en "FSANITIZE = -fsanitize=address\n\n" >> $FILE
+			echo -en "CFLAGS = -Wall -Wextra -Werror -g3\n\n" >> $FILE
+			echo -en "INC = -I include\n\n" >> $FILE
+			echo -en "FILE_DIR = src\n\n" >> $FILE
+			echo -en "SRC = \$(shell find \$(FILE_DIR) -type f -name '*.c')\n\n" >> $FILE
+			echo -en "OBJ_FOLDER = object_files\n\n" >> $FILE
+			echo -en "OBJ_SRC = \$(patsubst %.c, \$(OBJ_FOLDER)/%.o, \$(SRC))\n\n" >> $FILE
+			echo -en "LIBFT_DIR = libft/libft.a\n\n" >> $FILE
+
+			echo -en "# check for OS\n" >> $FILE
+			echo -en "ifeq (\$(shell uname), Linux)\n" >> $FILE
+			echo -en "\tMLX_DIR = minilibx-linux\n" >> $FILE
+			echo -en "\tMINILIBX_LIBRARY = -L\$(MINILIBX) -lmlx -L/usr/lib -Imlx_linux -lXext -lX11 -lm -lz\n" >> $FILE
+			echo -en "else\n" >> $FILE
+			echo -en "\tMLX_DIR = minilibx\n" >> $FILE
+			echo -en "\tMINILIBX_LIBRARY = -L\$(MLX_DIR) -lmlx -framework OpenGL -framework AppKit\n" >> $FILE
+			echo -en "endif\n\n" >> $FILE
+
+			printf "GREEN = \\\033[0;32m\n" >> $FILE
+			printf "RESET = \\\033[0m\n\n" >> $FILE
+
+			echo -en "all : \$(NAME)\n\n" >> $FILE
+
+			echo -en "fsan: fclean \$(OBJ_SRC)\n" >> $FILE
+			echo -en "\tmake -C \$(MLX_DIR)\n" >> $FILE
+			echo -en "\tmake -C libft\n" >> $FILE
+			echo -en "\t\$(CC) \$(CFLAGS) \$(FSANITIZE) \$(OBJ_SRC) \$(INC) \$(LIBFT_DIR)-o \$(NAME)\n" >> $FILE
+			echo -en "\t@echo \"\${GREEN}-----COMPILED FSAN DONE-----\\\n\${RESET}\"\n\n" >> $FILE
+
+			echo -en "\$(NAME) : \$(OBJ_SRC)\n" >> $FILE
+			echo -en "\tmake -C \$(MLX_DIR)\n" >> $FILE
+			echo -en "\tmake -C libft\n" >> $FILE
+			echo -en "\t\$(CC) \$(CFLAGS) \$(OBJ_SRC) \$(INC) \$(LIBFT_DIR) -o \$(NAME)\n" >> $FILE
+			echo -en "\t@echo \"\${GREEN}-----COMPILED DONE-----\\\n\${RESET}\"\n\n" >> $FILE
+
+			echo -en "\$(OBJ_FOLDER)/%.o : %.c\n" >> $FILE
+			echo -en "\t@mkdir -p \$(OBJ_FOLDER)\n" >> $FILE
+			echo -en "\t\$(CC) \$(CFLAGS) -c \$< -o \$@\n\n" >> $FILE
+
+			echo -en "clean :\n" >> $FILE
+			echo -en "\trm -rf \$(OBJ_FOLDER)\n" >> $FILE
+			echo -en "\tmake clean -C libft\n" >> $FILE
+			echo -en "\tmake clean -C \$(MLX_DIR)\n" >> $FILE
+
+			echo -en "fclean : clean\n" >> $FILE
+			echo -en "\tmake fclean -C libft\n" >> $FILE
+			echo -en "\tmake fclean -C \$(MLX_DIR)\n" >> $FILE
+			echo -en "\trm -f \$(NAME)\n" >> $FILE
+			echo -en "\t@echo \"\${GREEN}-----REMOVED ALL FILES-----\\\n\${RESET}\"\n\n" >> $FILE
+
+			echo -en "re : fclean all\n\n" >> $FILE
+
+			echo -en ".PHONY: all clean fclean re\n" >> $FILE
+		else
+			echo -en "FSANITIZE = -fsanitize=address\n\n" >> $FILE
+			echo -en "CFLAGS = -Wall -Wextra -Werror -g3\n\n" >> $FILE
+			echo -en "INC = -I include\n\n" >> $FILE
+			echo -en "FILE_DIR = src\n\n" >> $FILE
+			echo -en "SRC = \$(shell find \$(FILE_DIR) -type f -name '*.c')\n\n" >> $FILE
+			echo -en "OBJ_FOLDER = object_files\n\n" >> $FILE
+			echo -en "OBJ_SRC = \$(patsubst %.c, \$(OBJ_FOLDER)/%.o, \$(SRC))\n\n" >> $FILE
+			echo -en "LIBFT_DIR = libft/libft.a\n\n" >> $FILE
+
+			printf "GREEN = \\\033[0;32m\n" >> $FILE
+			printf "RESET = \\\033[0m\n\n" >> $FILE
+
+			echo -en "all : \$(NAME)\n\n" >> $FILE
+
+			echo -en "fsan: fclean $(OBJ_SRC)\n" >> $FILE
+			echo -en "\tmake -C libft\n" >> $FILE
+			echo -en "\t\$(CC) \$(CFLAGS) \$(FSANITIZE) \$(OBJ_SRC) \$(INC) \$(LIBFT_DIR)-o \$(NAME)\n" >> $FILE
+			echo -en "\t@echo \"\${GREEN}-----COMPILED FSAN DONE-----\\\n\${RESET}\"\n\n" >> $FILE
+
+			echo -en "\$(NAME) : \$(OBJ_SRC)\n" >> $FILE
+			echo -en "\tmake -C libft\n" >> $FILE
+			echo -en "\t\$(CC) \$(CFLAGS) \$(OBJ_SRC) \$(INC) \$(LIBFT_DIR) -o \$(NAME)\n" >> $FILE
+			echo -en "\t@echo \"\${GREEN}-----COMPILED DONE-----\\\n\${RESET}\"\n\n" >> $FILE
+
+			echo -en "\$(OBJ_FOLDER)/%.o : %.c\n" >> $FILE
+			echo -en "\t@mkdir -p \$(OBJ_FOLDER)\n" >> $FILE
+			echo -en "\t\$(CC) \$(CFLAGS) -c \$< -o \$@\n\n" >> $FILE
+
+			echo -en "clean :\n" >> $FILE
+			echo -en "\trm -rf \$(OBJ_FOLDER)\n" >> $FILE
+			echo -en "\tmake clean -C libft\n\n" >> $FILE
+
+			echo -en "fclean : clean\n" >> $FILE
+			echo -en "\tmake fclean -C libft\n" >> $FILE
+			echo -en "\trm -f \$(NAME)\n" >> $FILE
+			echo -en "\t@echo \"\${GREEN}-----REMOVED ALL FILES-----\\\n\${RESET}\"\n\n" >> $FILE
+
+			echo -en "re : fclean all\n\n" >> $FILE
+
+			echo -en ".PHONY: all clean fclean re\n" >> $FILE
+		fi
+	fi
 }
 
 echo -en " __  __ _____    ____ _____ _   _ _____ ____     _  _____ ___  ____  \n"
@@ -87,7 +215,6 @@ echo -en "| |\/| | |_    | |  _|  _| |  \| |  _| | |_) | / _ \ | || | | | |_) |\
 echo -en "| |  | |  _|   | |_| | |___| |\  | |___|  _ < / ___ \| || |_| |  _ < \n"
 echo -en "|_|  |_|_|      \____|_____|_| \_|_____|_| \_/_/   \_|_| \___/|_| \_\ \n\n"
 
-touch $FILE
 
 #questions
 echo -en "Project name: "
@@ -95,14 +222,23 @@ read project_input
 echo -en "Compiler: "
 read compiler_input
 
-echo -en "NAME = $project_input\n\n" >> $FILE
-echo -en "CC = $compiler_input\n\n" >> $FILE
-ft_input_the_rest
+# Prompt for bonus input outside of ft_input_the_rest
+read -p "Do you need bonus section? (y/n): " bonus_input
+while [[ ! "$bonus_input" =~ ^[yYnN]$ ]]; do
+    echo "Invalid input. Please answer y or n."
+    read -p "Do you need bonus section? (y/n): " bonus_input
+done
+
+# Prompt for minilibx input
+read -p "Do you need minilibx? (y/n): " minilibx_input
+while [[ ! "$minilibx_input" =~ ^[yYnN]$ ]]; do
+	echo "Invalid input. Please answer y or n."
+	read -p "Do you need minilibx? (y/n): " minilibx_input
+done
 
 #questions
 echo -en "\rEnter the directory for Makefile? (default: current directory): "
 read path_input
-ft_check
 
 #animation progress bar
 echo -en "Loading..."
@@ -118,3 +254,28 @@ sleep 1
 echo -en "\r██████████ 100%"
 sleep 1
 echo -en "\rMakefile created\n"
+
+touch $FILE
+
+echo -en "NAME = $project_input\n\n" >> $FILE
+echo -en "CC = $compiler_input\n\n" >> $FILE
+ft_input_the_rest
+
+# Optional: Move the Makefile if a custom directory is specified
+if [ -n "$path_input" ] && [ "$path_input" != "./" ]; then
+    # If a custom path is provided
+    mkdir -p "$path_input/src"
+    mkdir -p "$path_input/include"
+    if [[ "$bonus_input" =~ ^[yY]$ ]]; then
+        mkdir -p "$path_input/bonus"
+    fi
+    mv ./Makefile "$path_input/Makefile"
+else
+    # If no path is provided, create directories in the current directory
+    mkdir -p ./src
+    mkdir -p ./include
+    if [[ "$bonus_input" =~ ^[yY]$ ]]; then
+        mkdir -p ./bonus
+    fi
+    echo "No directory specified. Makefile and directories created in the current directory."
+fi
